4eca550cebab290ba4a289d878fc9a422efcc501
RahmaSoui <rahma.souissi.8@gmail.com>
2021-12-02 17:39:22 +0100

    full navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import Contact from "./pages/Contact/Contact";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Services from "./pages/Service/Services";
+import Testmonial from "./pages/Testmonial/Testmonial";
+import Navbar from "./Components/Navbar/Navbar";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+
+
+
+
+      <main>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/about" element={<About />} exact />
+        <Route path="/services" element={<Services />} exact />
+        <Route path="/testmonial" element={<Testmonial />} exact />
+        <Route path="/contact" element={<Contact />} exact />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+      
+      </main>
+  </Router>
   );
 }
 
